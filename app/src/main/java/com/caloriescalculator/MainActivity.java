19249ca08d9249ba59c9f79a83a12b93ec47525f
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-03-11 21:44:40 +0300

    First commit

---
+++ app/src/main/java/com/caloriescalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.caloriescalculator;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+@SuppressLint("Registered")
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
