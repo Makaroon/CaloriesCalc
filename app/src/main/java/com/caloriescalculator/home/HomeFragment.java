5bd90535a23246520f2170d82f37e86a5c8adf0c
Alexander Yasinovskiy <sa6ayas@gmail.com>
2019-05-09 02:24:28 +0300

    some improvements 3.2.5

--- app/src/main/java/com/caloriescalculator/home/HomeFragment.java
+++ app/src/main/java/com/caloriescalculator/home/HomeFragment.java
@@ -3,8 +3,6 @@ package com.caloriescalculator.home;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +10,6 @@ import android.view.ViewGroup;
 
 import com.caloriescalculator.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,26 +27,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.home_rv);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
-        rv_list = new ArrayList<>();
-        rv_list.add(new HomeItem("Home", R.drawable.ic_home_black_24dp));
-        rv_list.add(new HomeItem("Dashboard", R.drawable.ic_history));
-        rv_list.add(new HomeItem("Notification", R.drawable.ic_achievements));
-        rv_list.add(new HomeItem("image", R.drawable.ic_image));
-        rv_list.add(new HomeItem("Music video", R.drawable.ic_music_video));
-        rv_list.add(new HomeItem("Settings", R.drawable.ic_settings));
-
-
-        HomeRecyclerAdapter mAdapter = new HomeRecyclerAdapter(rv_list);
-
-        recyclerView.setAdapter(mAdapter);
-
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
         return view;
     }
 
